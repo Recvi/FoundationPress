7ba6c2ea7d1bb43bbc839259eee70e9a5b561263
Recvi <th.charalampos@gmail.com>
2016-09-26 11:09:13 +0300

    Revert "Hiding previous sidebar."
    This reverts commit aa7b98e20f936c7fbf23ed1b2b15b7aa1572145f.

--- page-templates/page-sidebar-left.php
+++ page-templates/page-sidebar-left.php
@@ -6,10 +6,10 @@ get_header(); ?>
 
 <?php get_template_part( 'template-parts/featured-image' ); ?>
 
-<!-- <div id="page-sidebar-left" role="main"> -->
+<div id="page-sidebar-left" role="main">
 
 <?php do_action( 'foundationpress_before_content' ); ?>
-<!-- <?php while ( have_posts() ) : the_post(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
       <header>
           <h1 class="entry-title"><?php the_title(); ?></h1>
@@ -21,15 +21,15 @@ get_header(); ?>
       <footer>
           <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
           <p><?php the_tags(); ?></p>
-      </footer> -->
+      </footer>
       <?php do_action( 'foundationpress_page_before_comments' ); ?>
-      <!-- <?php comments_template(); ?> -->
+      <?php comments_template(); ?>
       <?php do_action( 'foundationpress_page_after_comments' ); ?>
-  <!-- </article> -->
-<!-- <?php endwhile;?> -->
+  </article>
+<?php endwhile;?>
 
 <?php do_action( 'foundationpress_after_content' ); ?>
-<!-- <?php get_sidebar(); ?> -->
+<?php get_sidebar(); ?>
 
 </div>
 
