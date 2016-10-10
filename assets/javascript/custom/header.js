2a6a82f276eb879dfe08585b891c06dd7d857075
Recvi <th.charalampos@gmail.com>
2016-10-10 13:48:21 +0300

    Remove Admin bar for everyone except admins.

--- assets/javascript/custom/header.js
+++ assets/javascript/custom/header.js
@@ -1,3 +1,9 @@
 // $("#offCanvasToggle").bind("opened.zf.offcanvas closed.zf.offcanvas", function(e) {
 //   $("#offCanvasToggle").toggleClass("fi-x menu-icon");
 // });
+add_action('after_setup_theme', 'remove_admin_bar');
+function remove_admin_bar() {
+  if (!current_user_can('administrator') && !is_admin()) {
+    show_admin_bar(false);
+  }
+}
