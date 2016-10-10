e4be3e78338ba764827c94543f77e068f2a55655
Recvi <th.charalampos@gmail.com>
2016-10-10 14:10:40 +0300

    If you wanna do business you have require files!

--- functions.php
+++ functions.php
@@ -47,5 +47,8 @@ require_once( 'library/sticky-posts.php' );
 /** Configure responsive image sizes */
 require_once( 'library/responsive-images.php' );
 
+/** Remove admin bar to all users except administrators */
+require_once( 'library/remove-admin-bar.php');
+
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/protocol-relative-theme-assets.php' );
