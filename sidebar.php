dc8ff09da58c6755eb3af11d71b8ba62b4eddd78
recvi <th.charalampos@gmail.com>
2016-10-13 13:03:21 +0300

    Adding the sidebar menu.

--- sidebar.php
+++ sidebar.php
@@ -9,6 +9,7 @@
 ?>
 <aside class="sidebar">
 	<?php do_action( 'foundationpress_before_sidebar' ); ?>
-	<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
+	<!-- <?php dynamic_sidebar( 'sidebar-widgets' ); ?> -->
+	<?php foundationpress_mobile_under_nav(); ?>
 	<?php do_action( 'foundationpress_after_sidebar' ); ?>
 </aside>
