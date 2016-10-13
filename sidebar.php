02f19ba5decc582af5658fafbdc94651691a9950
Recvi <th.charalampos@gmail.com>
2016-10-14 01:58:08 +0300

    Enabling the new menu position.

--- sidebar.php
+++ sidebar.php
@@ -9,9 +9,6 @@
 ?>
 <aside class="sidebar">
 	<?php do_action( 'foundationpress_before_sidebar' ); ?>
-	<!-- <?php foundationpress_mobile_under_nav(); ?> -->
-	<ul class="vertical dropdown menu" id="sidebar-menu" data-dropdown-menu>
-		<li><a href=""></a></li>
-	</ul>
+	<?php foundationpress_mobile_under_nav(); ?>
 	<?php do_action( 'foundationpress_after_sidebar' ); ?>
 </aside>
