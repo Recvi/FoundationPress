db1aead9946edbc6708fe1be63e9823687182951
Recvi <th.charalampos@gmail.com>
2016-10-14 09:18:27 +0300

    Corrected searchform position.

--- header.php
+++ header.php
@@ -53,11 +53,14 @@
 			</div>
 			<div class="top-bar-right">
 				<?php foundationpress_top_bar_r(); ?>
-				<?php get_search_form(); ?>
+
 				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'topbar' ) : ?>
 					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
 				<?php endif; ?>
 			</div>
+
+			<?php get_search_form(); ?>
+			
 		</nav>
 
 	</header>
