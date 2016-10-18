29c4ceb5f6ea8784b5d0fbb7f175172e94b43249
Recvi <th.charalampos@gmail.com>
2016-10-18 10:37:21 +0300

    Whoops, has to be inside main content.

--- page-templates/page-sidebar-left.php
+++ page-templates/page-sidebar-left.php
@@ -11,39 +11,39 @@ get_header(); ?>
 
 <?php do_action( 'foundationpress_before_content' ); ?>
 
-<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-  <ul class="orbit-container">
-    <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-    <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-    <li class="is-active orbit-slide">
-      <img class="orbit-image" src="assets/images/carlson/orbit/01.jpg" alt="Space">
-      <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
-    </li>
-    <li class="orbit-slide">
-      <img class="orbit-image" src="assets/images/carlson/orbit/02.jpg" alt="Space">
-      <figcaption class="orbit-caption">Lets Rocket!</figcaption>
-    </li>
-    <li class="orbit-slide">
-      <img class="orbit-image" src="assets/images/carlson/orbit/03.jpg" alt="Space">
-      <figcaption class="orbit-caption">Encapsulating</figcaption>
-    </li>
-    <li class="orbit-slide">
-      <img class="orbit-image" src="assets/images/carlson/orbit/04.jpg" alt="Space">
-      <figcaption class="orbit-caption">Outta This World</figcaption>
-    </li>
-  </ul>
-  <nav class="orbit-bullets">
-    <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-    <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-    <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-    <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-  </nav>
-</div>
 <?php while ( have_posts() ) : the_post(); ?>
   <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
       <header>
           <h1 class="entry-title"><?php the_title(); ?></h1>
       </header>
+      <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+        <ul class="orbit-container">
+          <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+          <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+          <li class="is-active orbit-slide">
+            <img class="orbit-image" src="assets/images/carlson/orbit/01.jpg" alt="Space">
+            <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
+          </li>
+          <li class="orbit-slide">
+            <img class="orbit-image" src="assets/images/carlson/orbit/02.jpg" alt="Space">
+            <figcaption class="orbit-caption">Lets Rocket!</figcaption>
+          </li>
+          <li class="orbit-slide">
+            <img class="orbit-image" src="assets/images/carlson/orbit/03.jpg" alt="Space">
+            <figcaption class="orbit-caption">Encapsulating</figcaption>
+          </li>
+          <li class="orbit-slide">
+            <img class="orbit-image" src="assets/images/carlson/orbit/04.jpg" alt="Space">
+            <figcaption class="orbit-caption">Outta This World</figcaption>
+          </li>
+        </ul>
+        <nav class="orbit-bullets">
+          <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
+          <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
+          <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
+          <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
+        </nav>
+      </div>
       <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
       <div class="entry-content">
           <?php the_content(); ?>
