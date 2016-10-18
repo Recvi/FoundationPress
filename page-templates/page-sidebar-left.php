23d196501c6bc888ad742c18e40d42e71d6a8426
Recvi <th.charalampos@gmail.com>
2016-10-18 14:45:24 +0300

    Refactoring page-sidebar-left layout.

--- page-templates/page-sidebar-left.php
+++ page-templates/page-sidebar-left.php
@@ -10,47 +10,35 @@ get_header(); ?>
 
 
 <?php do_action( 'foundationpress_before_content' ); ?>
-
-<?php while ( have_posts() ) : the_post(); ?>
-  <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-      <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-        <ul class="orbit-container">
-          <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-          <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-          <li class="is-active orbit-slide">
-            <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/01.jpg'?>" alt="Space">
-            <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
-          </li>
-          <li class="orbit-slide">
-            <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/02.jpg'?>" alt="Space">
-            <figcaption class="orbit-caption">Lets Rocket!</figcaption>
-          </li>
-          <li class="orbit-slide">
-            <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/03.jpg'?>" alt="Space">
-            <figcaption class="orbit-caption">Encapsulating</figcaption>
-          </li>
-          <li class="orbit-slide">
-            <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/04.jpg'?>" alt="Space">
-            <figcaption class="orbit-caption">Outta This World</figcaption>
-          </li>
-        </ul>
-        <nav class="orbit-bullets">
-          <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-          <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-          <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-          <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-        </nav>
-      </div>
-      <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-      <div class="entry-content">
-          <?php the_content(); ?>
-      </div>
-      <footer>
-          <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
-          <p><?php the_tags(); ?></p>
-      </footer>
-  </article>
-<?php endwhile;?>
+<div class="main-content">
+  <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+    <ul class="orbit-container">
+      <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+      <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+      <li class="is-active orbit-slide">
+        <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/01.jpg'?>" alt="Space">
+        <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
+      </li>
+      <li class="orbit-slide">
+        <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/02.jpg'?>" alt="Space">
+        <figcaption class="orbit-caption">Lets Rocket!</figcaption>
+      </li>
+      <li class="orbit-slide">
+        <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/03.jpg'?>" alt="Space">
+        <figcaption class="orbit-caption">Encapsulating</figcaption>
+      </li>
+      <li class="orbit-slide">
+        <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/04.jpg'?>" alt="Space">
+        <figcaption class="orbit-caption">Outta This World</figcaption>
+      </li>
+    </ul>
+    <nav class="orbit-bullets">
+      <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
+      <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
+      <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
+      <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
+    </nav>
+  </div>
 <?php
   $args = array('posts_per_page'=> 5, 'orderby'=> 'date');
   $postlist = get_posts($args);
@@ -72,6 +60,7 @@ get_header(); ?>
     wp_reset_postdata();
 ?>
 <?php do_action( 'foundationpress_after_content' ); ?>
+</div>
 <?php get_sidebar(); ?>
 
 </div>
