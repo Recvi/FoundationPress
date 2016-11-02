24d17fc117b31364dc559f203d313c1abf8107f2
Recvi <th.charalampos@gmail.com>
2016-11-02 15:52:43 +0200

    Added icon inside link.

--- assets/javascript/custom/sidebar-icons.js
+++ assets/javascript/custom/sidebar-icons.js
@@ -1,2 +1,2 @@
-$("#menu-item-34 > a").prepend("<img src=\"wp-content/themes/FoundationPress/assets/images/carlson/signin.png\" alt=\"CWT Functions\" \\>");
-$("#menu-item-59 > a").prepend("<img src=\"wp-content/themes/FoundationPress/assets/images/carlson/links.png\" alt=\"CWT Functions\" \\>");
+$("#menu-item-34 > a").prepend("<img src=\"wp-content/themes/FoundationPress/assets/images/carlson/signin.png\" alt=\"CWT Functions\"><br>");
+$("#menu-item-59 > a").prepend("<img src=\"wp-content/themes/FoundationPress/assets/images/carlson/links.png\" alt=\"Helpful Links\"><br>");
