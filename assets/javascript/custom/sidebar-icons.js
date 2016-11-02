eac3212b59e1f1bb20438e6fc7c1b2e68430476d
Recvi <th.charalampos@gmail.com>
2016-11-02 15:40:49 +0200

    Added links icon.

--- assets/javascript/custom/sidebar-icons.js
+++ assets/javascript/custom/sidebar-icons.js
@@ -1,1 +1,2 @@
 $("#menu-item-34").prepend("<img src=\"wp-content/themes/FoundationPress/assets/images/carlson/signin.png\" alt=\"CWT Functions\" \\>");
+$("#menu-item-59").prepend("<img src=\"wp-content/themes/FoundationPress/assets/images/carlson/links.png\" alt=\"CWT Functions\" \\>");
