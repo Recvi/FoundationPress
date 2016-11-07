389c62503615c422b51dc98d3599573bfc847305
Recvi <th.charalampos@gmail.com>
2016-11-07 12:38:35 +0200

    Added submenu headers.

--- assets/javascript/custom/sidebar-icons.js
+++ assets/javascript/custom/sidebar-icons.js
@@ -1,2 +1,4 @@
 $("#menu-item-34 > a").prepend("<img src=\"wp-content/themes/FoundationPress/assets/images/carlson/signin.png\" alt=\"CWT Functions\"><br>");
+$("#menu-item-34 > ul").prepend("<h3 class=\"submenu-header\">CWT FUNCTIONS</h3>");
 $("#menu-item-59 > a").prepend("<img src=\"wp-content/themes/FoundationPress/assets/images/carlson/links.png\" alt=\"Helpful Links\"><br>");
+$("#menu-item-59 > ul").prepend("<h3 class=\"submenu-header\">SUPPLIER</h3>");
