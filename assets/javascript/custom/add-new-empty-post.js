89b040a9931c63da2b43656d3345615d0ed5d00a
Recvi <th.charalampos@gmail.com>
2016-11-16 13:09:15 +0200

    Functionality that creates a new post and redirects to the edit page DONE and tested.

--- assets/javascript/custom/add-new-empty-post.js
+++ assets/javascript/custom/add-new-empty-post.js
@@ -1,5 +1,8 @@
 $( "#create-empty-post" ).click(function() {
   var post = new wp.api.models.Post({
-      title: $( "#post-title" ).getText??
+      title: $( "#new-post-title" ).val();
+  });
+  post.save().done(function(res) {
+    window.location.replace("http://"+window.location.hostname+"/?p="+res.id+"&preview=true");
   });
 });
