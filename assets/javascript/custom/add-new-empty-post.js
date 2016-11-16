4adda74cc93d767fce252b5c8133d311c4ae89c4
Recvi <th.charalampos@gmail.com>
2016-11-16 13:11:37 +0200

    Hmm, it seems I can't add ';' inside object declaration. NOTED

--- assets/javascript/custom/add-new-empty-post.js
+++ assets/javascript/custom/add-new-empty-post.js
@@ -1,8 +1,8 @@
-$( "#create-empty-post" ).click(function() {
+$( "#create-empty-post" ).click( function() {
   var post = new wp.api.models.Post({
-      title: $( "#new-post-title" ).val();
+      title: $( "#new-post-title" ).val()
   });
-  post.save().done(function(res) {
+  post.save().done( function(res) {
     window.location.replace("http://"+window.location.hostname+"/?p="+res.id+"&preview=true");
   });
 });
