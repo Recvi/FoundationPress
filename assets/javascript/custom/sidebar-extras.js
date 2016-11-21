5c4a2ec11c3782c2f77cf0c58f56fec888d3ad92
Recvi <th.charalampos@gmail.com>
2016-11-21 12:14:46 +0200

    Whoopsie, wrong id for the General Info.

--- assets/javascript/custom/sidebar-extras.js
+++ assets/javascript/custom/sidebar-extras.js
@@ -3,6 +3,6 @@ $("#menu-item-34 > a").prepend("<img src=\""+imgUrl+"signin.png\" alt=\"CWT Func
 $("#menu-item-34 > ul").prepend("<h3 class=\"submenu-header\">CWT FUNCTIONS</h3>");
 $("#menu-item-59 > a").prepend("<img src=\""+imgUrl+"supplier.png\" alt=\"Supplier\"><br>");
 $("#menu-item-59 > ul").prepend("<h3 class=\"submenu-header\">SUPPLIER</h3>");
-$("#menu-item-59 > a").prepend("<img src=\""+imgUrl+"info.png\" alt=\"Info\"><br>");
+$("#menu-item-169 > a").prepend("<img src=\""+imgUrl+"info.png\" alt=\"Info\"><br>");
 $("#menu-item-165 > a").prepend("<img src=\""+imgUrl+"links.png\" alt=\"Useful Links\"><br>");
 $("#menu-item-165 > ul").prepend("<h3 class=\"submenu-header\">USEFUL LINKS</h3>");
