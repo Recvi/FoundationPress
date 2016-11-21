dff6bd924203e3ce48ea0539b77687b173f908d1
Recvi <th.charalampos@gmail.com>
2016-11-21 12:13:44 +0200

    Ready with titles and icons.

--- assets/javascript/custom/sidebar-extras.js
+++ assets/javascript/custom/sidebar-extras.js
@@ -1,7 +1,8 @@
 var imgUrl = "//"+window.location.hostname+"/wp-content/themes/FoundationPress/assets/images/carlson/";
 $("#menu-item-34 > a").prepend("<img src=\""+imgUrl+"signin.png\" alt=\"CWT Functions\"><br>");
 $("#menu-item-34 > ul").prepend("<h3 class=\"submenu-header\">CWT FUNCTIONS</h3>");
-$("#menu-item-59 > a").prepend("<img src=\""+imgUrl+"suppliers.png\" alt=\"Supplier\"><br>");
+$("#menu-item-59 > a").prepend("<img src=\""+imgUrl+"supplier.png\" alt=\"Supplier\"><br>");
 $("#menu-item-59 > ul").prepend("<h3 class=\"submenu-header\">SUPPLIER</h3>");
+$("#menu-item-59 > a").prepend("<img src=\""+imgUrl+"info.png\" alt=\"Info\"><br>");
 $("#menu-item-165 > a").prepend("<img src=\""+imgUrl+"links.png\" alt=\"Useful Links\"><br>");
 $("#menu-item-165 > ul").prepend("<h3 class=\"submenu-header\">USEFUL LINKS</h3>");
