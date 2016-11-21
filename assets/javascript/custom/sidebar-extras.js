3d2896f311fb43b39fc61f53a14250b681856ac6
Recvi <th.charalampos@gmail.com>
2016-11-21 13:26:25 +0200

    More titles on sidebar.

--- assets/javascript/custom/sidebar-extras.js
+++ assets/javascript/custom/sidebar-extras.js
@@ -4,5 +4,6 @@ $("#menu-item-34 > ul").prepend("<h3 class=\"submenu-header\">CWT FUNCTIONS</h3>
 $("#menu-item-59 > a").prepend("<img src=\""+imgUrl+"supplier.png\" alt=\"Supplier\"><br>");
 $("#menu-item-59 > ul").prepend("<h3 class=\"submenu-header\">SUPPLIER</h3>");
 $("#menu-item-169 > a").prepend("<img src=\""+imgUrl+"info.png\" alt=\"Info\"><br>");
+$("#menu-item-169 > ul").prepend("<h3 class=\"submenu-header\">GENERAL INFO</h3>");
 $("#menu-item-165 > a").prepend("<img src=\""+imgUrl+"links.png\" alt=\"Useful Links\"><br>");
 $("#menu-item-165 > ul").prepend("<h3 class=\"submenu-header\">USEFUL LINKS</h3>");
