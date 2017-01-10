a19c97a29cd7f36884b5991203b134a3bb54e29d
Recvi <th.charalampos@gmail.com>
2017-01-10 23:03:58 +0200

    Customizing front page.

--- page-templates/front.php
+++ page-templates/front.php
@@ -40,6 +40,19 @@ get_header(); ?>
     </nav>
   </div>
   <!-- TODO: Show the last 5 posts by date in a fixed list. -->
+  <div id="timeline">
+    <ul>
+    <?php
+      $recent_posts = wp_get_recent_posts();
+      foreach( $recent_posts as $recent ) {
+        printf( '<li><a href="%1$s">%2$s</a></li>',
+            esc_url( get_permalink( $recent['ID'] ) ),
+            apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
+          );
+      }
+     ?>
+   </ul>
+  </div>
 <?php the_content(); ?>
 <?php do_action( 'foundationpress_after_content' ); ?>
 </div>
