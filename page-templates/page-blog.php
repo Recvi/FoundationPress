6dcec44ff904b4c3ede6a46e01e2140f24fa9e65
Recvi <th.charalampos@gmail.com>
2017-01-12 13:11:02 +0200

    Go!

--- page-templates/page-blog.php
+++ page-templates/page-blog.php
@@ -14,7 +14,7 @@ get_header(); ?>
 <div class="page-image">
   <img src="<?php echo get_template_directory_uri(). '/../../uploads/2016/11/panel.enscale.TravelTechnology-SmallHero.full_.high_-1.jpg'; ?>" alt="Header image"  />
   <h2 class="page-image-title"><?php the_title(); ?></h2>
-  <small style="position:absolute; top:300px; color:#fefefe; text-align:center;">This is where you can see all the blog posts. You can create one by selecting the "Create New Post" option in the sidebar.</small>
+  <small style="position:absolute; top:300px; color:#fefefe; width:100%; text-align:center;">This is where you can see all the blog posts. You can create one by selecting the "Create New Post" option in the sidebar.</small>
 </div>
 <small>Disclaimer: Content is strictly personal. It does not reflect the views of Carlson Wagonlit Travel and shall be fully compliant to company privacy.</small>
 <?php $args = array('posts_per_page'=> 5, 'orderby'=> 'date');
