e19e72a6e813fa87bba68ad0a0b6c28296967d50
Recvi <th.charalampos@gmail.com>
2017-01-12 12:47:44 +0200

    The sad part about it is that it was always correct.

--- page-templates/page-blog.php
+++ page-templates/page-blog.php
@@ -12,23 +12,30 @@ get_header(); ?>
 
 <div class="page-image">
   <img src="<?php echo get_template_directory_uri(). '/../../uploads/2016/11/panel.enscale.TravelTechnology-SmallHero.full_.high_-1.jpg'; ?>" alt="Header image"  />
-  <!-- <h2 class="page-image-title"><?php the_title(); ?></h2> -->
+  <h2 class="page-image-title"><?php the_title(); ?></h2>
 </div>
-<?php while ( have_posts() ) : the_post(); ?>
-  <article <?php post_class('main-content') ?> id="post-<?php the_ID(); ?>">
-      <header>
-          <h1 class="entry-title"><?php the_title(); ?></h1>
-      </header>
-      <?php do_action( 'foundationpress_page_before_entry_content' ); ?>
-      <div class="entry-content">
-          <?php the_content(); ?>
-      </div>
-      <footer>
-          <?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+<?php $args = array('posts_per_page'=> 5, 'orderby'=> 'date');
+  $postlist = get_posts($args);
+  foreach ($postlist as $post):
+    setup_postdata($post); ?>
+      <article id="post-<?php the_ID(); ?>">
+        <header>
+          <h1 class="entry-title">
+            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+          </h1>
+        </header>
+        <div class="entry-content">
+          <?php the_excerpt(); ?>
+        </div>
+        <footer>
           <p><?php the_tags(); ?></p>
-      </footer>
-  </article>
-<?php endwhile;?>
+        </footer>
+      </article>
+<?php
+  endforeach;
+  wp_reset_postdata();
+?>
+<?php the_content(); ?>
 <?php do_action( 'foundationpress_after_content' ); ?>
 <?php get_sidebar(); ?>
 
