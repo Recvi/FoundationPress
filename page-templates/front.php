3194f2f1dfb9aff62ea19902f47412175306c1c9
Recvi <th.charalampos@gmail.com>
2017-01-12 16:20:47 +0200

    General changes

--- page-templates/front.php
+++ page-templates/front.php
@@ -13,33 +13,12 @@ get_header(); ?>
 
 <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
     <ul class="orbit-container">
-      <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-      <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
       <li class="is-active orbit-slide">
         <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/01.jpg'?>" alt="Space">
-        <figcaption class="orbit-caption my-caption">Welcome, to your new Intranet!</figcaption>
-      </li>
-      <li class="orbit-slide">
-        <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/02.jpg'?>" alt="Space">
-        <figcaption class="orbit-caption my-caption">It contains everything you need to succeed!</figcaption>
-      </li>
-      <li class="orbit-slide">
-        <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/03.jpg'?>" alt="Space">
-        <figcaption class="orbit-caption my-caption">And allows you to express yourself.</figcaption>
-      </li>
-      <li class="orbit-slide">
-        <img class="orbit-image" src="<?php echo get_template_directory_uri().'/assets/images/carlson/orbit/04.jpg'?>" alt="Space">
-        <figcaption class="orbit-caption my-caption">Let's explore it.</figcaption>
+        <figcaption class="orbit-caption my-caption"><span class="smaller">Welcome to</span><br>CWT Greece Intranet</figcaption>
       </li>
     </ul>
-    <nav class="orbit-bullets">
-      <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-      <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-      <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-      <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-    </nav>
   </div>
-  <!-- TODO: Show the last 5 posts by date in a fixed list. -->
   <div id="timeline">
     <h4>Recent Posts</h4>
     <ul>
