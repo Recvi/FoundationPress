ba16f8b6567dd3f31809d1c1051ed6e0f86e5cc1
Recvi <th.charalampos@gmail.com>
2017-01-12 12:27:27 +0200

    Try this!

--- page-templates/page-blog.php
+++ page-templates/page-blog.php
@@ -14,24 +14,24 @@ get_header(); ?>
   <img src="<?php echo get_template_directory_uri(). '/../../uploads/2016/11/panel.enscale.TravelTechnology-SmallHero.full_.high_-1.jpg'; ?>" alt="Header image"  />
   <h2 class="page-image-title"><?php the_title(); ?></h2>
 </div>
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-      <article id="post-<?php the_ID(); ?>">
-        <header>
-          <h1 class="entry-title">
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-          </h1>
-        </header>
-        <div class="entry-content">
-          <?php the_excerpt(); ?>
-        </div>
-        <footer>
-          <p><?php the_tags(); ?></p>
-        </footer>
-      </article>
-<?php endwhile; else: ?>
-  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
-<?php the_content(); ?>
+<?php while ( have_posts() ) : the_post(); ?>
+<section class="intro" role="main">
+	<div class="fp-intro">
+
+		<div <?php post_class() ?> id="post-<?php the_ID(); ?>">
+			<?php do_action( 'foundationpress_page_before_entry_content' ); ?>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+			<footer>
+				<?php wp_link_pages( array('before' => '<nav id="page-nav"><p>' . __( 'Pages:', 'foundationpress' ), 'after' => '</p></nav>' ) ); ?>
+				<p><?php the_tags(); ?></p>
+			</footer>
+		</div>
+
+	</div>
+</section>
+<?php endwhile;?>
 <?php do_action( 'foundationpress_after_content' ); ?>
 <?php get_sidebar(); ?>
 
